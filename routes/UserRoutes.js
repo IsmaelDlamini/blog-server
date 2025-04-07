533046b02152a13be4172e3bef0a1b0cb37d1d80
Ismael <68245583+IsmaelDlamini@users.noreply.github.com>
2025-04-07 14:32:36 +0200

    Update UserRoutes.js
    added a route for getting all the user information

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { fetchUserInfo, logout, registerUser } from "../controllers/UserController.js";
+import { fetchUserInfo, logout, registerUser, getAllUsers } from "../controllers/UserController.js";
 import { login } from "../controllers/UserController.js";
 import { verifyToken } from "../config/jwtAuth.js";
 
 const userRouter = express.Router();
+
+// @desc get all users
+// @route GET /api/users/
+// @access Public
+
+userRouter.get("/", getAllUsers);
 
 // @desc Register user
 // @route GET /api/users/register
@@ -17,20 +23,17 @@ userRouter.post("/register", registerUser);
 
 userRouter.post("/login", login);
 
-
 // @desc get the user information
 // @route GET /api/users/userInfo
 // @access Public
 
 userRouter.get("/userInfo", verifyToken , fetchUserInfo);
 
-
 // @desc logout user
 // @route GET /api/users/logout
 // @access Public
 
 userRouter.post("/logout", logout);
-
 
 export default userRouter;
 
