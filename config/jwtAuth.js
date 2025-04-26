403ba2365fd98e1704cdc180898d491491d668ee
Ismail <iii409475@gmail.com>
2025-04-26 19:56:34 +0200

    Add checkForTokenWeak middleware to handle token validation and user assignment

--- config/jwtAuth.js
+++ config/jwtAuth.js
@@ -22,3 +22,25 @@ export const verifyToken = async (req, res, next) => {
     return res.status(500).json({ message: "Error Validating Token" });
   }
 };
+
+
+export const checkForTokenWeak = async (req, res, next) => {
+  try {
+    const token = req.cookies.token; // Read token from cookies
+
+    console.log("Token:", token); // Log the token
+
+    if (token) {
+      const decoded = await jwt.verify(token, process.env.SECRET_KEY);
+      // Attach the decoded user to the request
+      req.user = decoded;
+      next(); // Continue to the next middleware/route handler
+    } else {
+      req.user = { userId: null }; // Assign null userId if no token found
+      next(); // Continue even if no token is found
+    }
+
+  } catch (error) {
+    return res.status(500).json({ message: "Error Validating Token" });
+  }
+};
