954165650a7845c0eda5805d3fa4c461332efed5
Ismail <iii409475@gmail.com>
2025-05-01 21:22:18 +0200

    Add CommentRoutes for handling comment creation and likes

---
+++ routes/CommentRoutes.js
@@ -0,0 +1,20 @@
+
+import express from "express"
+import { CreateComment, fetchAllCommentsForPost, ToggleCommmentLike } from "../controllers/CommentController.js";
+import { checkForTokenWeak } from "../config/jwtAuth.js";
+
+const commentRouter = express.Router();
+
+// @desc create comment
+// @route /api/comments/add
+
+commentRouter.post("/create", CreateComment);
+commentRouter.get("/", checkForTokenWeak, fetchAllCommentsForPost);
+commentRouter.post("/toggleLike", ToggleCommmentLike)
+
+
+export default commentRouter;
+
+
+
+
